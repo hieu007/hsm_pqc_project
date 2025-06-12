e7a780a082a2adb600872bc6f247f75e796e0881
TRAN THE HIEU <hieubcy@gmail.com>
2025-06-12 15:30:49 +0900

    Update build_oqs_openssl.sh

--- HSM_FIRM/build_oqs_openssl.sh
+++ HSM_FIRM/build_oqs_openssl.sh
@@ -5,8 +5,6 @@ set -e
 
 sudo apt install -y git libssl-dev
 
-git clone --branch OQS-OpenSSL_1_1_1-stable https://github.com/open-quantum-safe/oqs-openssl.git
-cd oqs-openssl
-./Configure linux-x86_64 -lm
-make -j$(nproc)
-sudo make install
+git clone --branch OQS-OpenSSL_1_1_1-stable https://github.com/open-quantum-safe/oqs-provider.git
+cd oqs-provider
+env OQS_LIBJADE_BUILD="OFF" bash scripts/fullbuild.sh
