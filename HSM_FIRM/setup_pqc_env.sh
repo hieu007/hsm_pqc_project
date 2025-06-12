159972d1629563f7dd3ca589510ee66199da48ae
TRAN THE HIEU <hieubcy@gmail.com>
2025-06-12 11:23:46 +0900

    Update setup_pqc_env.sh

--- HSM_FIRM/setup_pqc_env.sh
+++ HSM_FIRM/setup_pqc_env.sh
@@ -1,18 +1,20 @@
-# Cài các gói cần thiết
-sudo apt update
-sudo apt install cmake build-essential ninja-build libssl-dev
+#!/bin/bash
+# Cài đặt liboqs, oqs-openssl và pkcs11-provider cho PQC + HSM test
 
-# Clone liboqs và build
+set -e  # Dừng nếu có lỗi
+
+sudo apt update
+sudo apt install -y build-essential cmake ninja-build git libssl-dev softhsm2
+
+# Clone liboqs
 git clone --recursive https://github.com/open-quantum-safe/liboqs.git
-cd liboqs
-mkdir build && cd build
-cmake -GNinja .. -DOQS_USE_OPENSSL=ON -DBUILD_SHARED_LIBS=ON
-ninja
-sudo ninja install
-# Clone và build OQS-OpenSSL (tùy chỉnh OpenSSL hỗ trợ PQC)
-cd ~
+cd liboqs && mkdir build && cd build
+cmake -GNinja .. -DBUILD_SHARED_LIBS=ON
+ninja && sudo ninja install
+cd ../..
+
+# Clone và build oqs-openssl
 git clone --branch OQS-OpenSSL_1_1_1-stable https://github.com/open-quantum-safe/oqs-openssl.git
 cd oqs-openssl
-./Configure linux-x86_64 -lm
-make -j$(nproc)
-sudo make install
+./Configure linux-x86_64
+make -j$(nproc) && sudo make install
