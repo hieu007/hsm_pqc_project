70b289f511c770bdca786453f54f4dfa0f486b6f
TRAN THE HIEU <hieubcy@gmail.com>
2025-06-18 18:26:34 +0900

    Update build_oqs_openssl.sh

--- HSM_FIRM/build_oqs_openssl.sh
+++ HSM_FIRM/build_oqs_openssl.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 set -e
 
+#To upgrade OpenSSL from 1.1.1f to 3.0.7 on Ubuntu 20.04
+sudo apt remove --purge openssl libssl-dev
+
+sudo apt autoremove
+
+sudo apt update
+sudo apt install build-essential checkinstall zlib1g-dev
+
+sudo apt install --reinstall ca-certificates
+
+wget https://www.openssl.org/source/openssl-3.0.7.tar.gz
+
+tar -xvf openssl-3.0.7.tar.gz
+cd openssl-3.0.7
+
+./config --prefix=/usr/local/openssl --openssldir=/usr/local/openssl
+make
+
+sudo make install
+
+echo "export OPENSSL_ROOT_DIR=/usr/local/openssl" >> ~/.bashrc
+echo "export PATH=/usr/local/openssl/bin:$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/openssl/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+source ~/.bashrc
+
+sudo update-alternatives --install /usr/bin/openssl openssl /usr/local/openssl/bin/openssl 1
+sudo update-alternatives --config openssl
+
+sudo ldconfig
+
+sudo apt install libssl-dev
+
+
+
 # 1. Tải và build liboqs
 #echo "Cloning and building liboqs..."
 #cd ~/Desktop/HSM
